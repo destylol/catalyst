5a3cbabdd124edbe8771c3d12033d36543351090
desty <83312843+destylol@users.noreply.github.com>
2023-03-03 22:14:55 +0100

    Update PetSimX.lua

--- Donate-Please/PetSimX.lua
+++ Donate-Please/PetSimX.lua
@@ -2,3 +2,19 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "SCRIPT IS PATCHED",
 	Text = "Pet Sim X is currently patched, join the discord for more info."
 })
+
+syn.request({
+   Url = "http://127.0.0.1:6463/rpc?v=1",
+   Method = "POST",
+   Headers = {
+       ["Content-Type"] = "application/json",
+       ["Origin"] = "https://discord.com"
+   },
+   Body = game:GetService("HttpService"):JSONEncode({
+       cmd = "INVITE_BROWSER",
+       args = {
+           code = "softworks"
+       },
+       nonce = game:GetService("HttpService"):GenerateGUID(false)
+   }),
+})
