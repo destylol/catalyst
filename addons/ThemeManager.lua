1f166312816b583d8e9dd01dc3aedc341f67636f
desty <83312843+destylol@users.noreply.github.com>
2023-04-15 20:07:30 +0200

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Catalyst Hub'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"221f1f","AccentColor":"7ccfff","BackgroundColor":"212121","OutlineColor":"302b2b"}') },
+		['Catalyst Hub'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"000b16","AccentColor":"3c85e0","BackgroundColor":"08080d","OutlineColor":"011b2b"}') },
 		['Dracula'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232533","AccentColor":"6271a5","BackgroundColor":"1b1c27","OutlineColor":"7c82a7"}') },
 		['Bitch Bot'] 		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Kiriot Hub'] 		= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"30333b","AccentColor":"ffaa00","BackgroundColor":"1a1c20","OutlineColor":"141414"}') },
